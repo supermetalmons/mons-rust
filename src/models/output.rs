4cb3ea097ef77ba813a425d5c9cc2854d91f721a
ivan grachev <ivan@ivan.lol>
2024-03-12 20:32:01 +0300

    fix json case

--- src/models/output.rs
+++ src/models/output.rs
@@ -1,6 +1,7 @@
 use crate::*;
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Hash, Clone)]
+#[serde(rename_all = "camelCase")]
 pub enum Output {
     InvalidInput,
     LocationsToStartFrom(Vec<Location>),
