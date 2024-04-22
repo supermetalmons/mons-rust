fb4cc4f00d57213e69e9dc612f93c07386f170d7
ivan grachev <ivan@ivan.lol>
2024-04-22 19:40:45 +0300

    expose game data to wasm

--- src/models/mons_game_model.rs
+++ src/models/mons_game_model.rs
@@ -21,4 +21,25 @@ impl MonsGameModel {
     pub fn fen(&self) -> String {
         return self.game.fen();
     }
+
+    pub fn active_color(&self) -> Color {
+        return self.game.active_color;
+    }
+
+    pub fn winner_color(&self) -> Option<Color> {
+        return self.game.winner_color();
+    }
+
+    pub fn black_score(&self) -> i32 {
+        return self.game.black_score;
+    }
+
+    pub fn white_score(&self) -> i32 {
+        return self.game.white_score;
+    }
+
+    pub fn available_move_kinds(&self) -> Vec<i32> {
+        let map = self.game.available_move_kinds();
+        return [map[&AvailableMoveKind::MonMove], map[&AvailableMoveKind::ManaMove], map[&AvailableMoveKind::Action], map[&AvailableMoveKind::Potion]].to_vec();
+    }
 }
