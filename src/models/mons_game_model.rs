7bd9b7ae0f4a084810baf621c188cd7131a59e6c
ivan grachev <ivan@ivan.lol>
2024-11-01 16:29:32 +0300

    modify existing game model on automove

--- src/models/mons_game_model.rs
+++ src/models/mons_game_model.rs
@@ -28,8 +28,7 @@ impl MonsGameModel {
 
     pub fn automove(&mut self) -> OutputModel {
         let mut inputs = Vec::new();
-        let mut game = self.game.clone();
-        let mut output = game.process_input(vec![], false, false);
+        let mut output = self.game.process_input(vec![], false, false);
 
         loop {
             match output {
@@ -43,7 +42,7 @@ impl MonsGameModel {
                     let random_index = self.random_index(locations.len());
                     let location = locations[random_index];
                     inputs.push(Input::Location(location));
-                    output = game.process_input(inputs.clone(), false, false);
+                    output = self.game.process_input(inputs.clone(), false, false);
                 }
                 Output::NextInputOptions(options) => {
                     if options.is_empty() {
@@ -52,11 +51,10 @@ impl MonsGameModel {
                     let random_index = self.random_index(options.len());
                     let next_input = options[random_index].input.clone();
                     inputs.push(next_input);
-                    output = game.process_input(inputs.clone(), false, false);
+                    output = self.game.process_input(inputs.clone(), false, false);
                 }
                 Output::Events(events) => {
                     let input_fen = Input::fen_from_array(&inputs);
-                    self.game = game;
                     return OutputModel::new(Output::Events(events), input_fen.as_str());
                 }
             }
