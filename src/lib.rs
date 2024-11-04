2b36e1558089f8e230c362af73779871f21bac37
ivan grachev <ivan@ivan.lol>
2024-11-04 13:58:03 +0300

    clean up tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 pub mod models;
 pub use models::*;
 
-// TODO: "x" stands for corrupted game data. see if there was cheating.
 #[wasm_bindgen]
 pub fn winner(fen_w: &str, fen_b: &str, flat_moves_string_w: &str, flat_moves_string_b: &str) -> String {
     let moves_w: Vec<&str> = flat_moves_string_w.split("-").collect();
@@ -62,93 +61,21 @@ pub fn winner(fen_w: &str, fen_b: &str, flat_moves_string_w: &str, flat_moves_st
         }
     }
 
+    // TODO: "x" stands for corrupted game data. see if there was cheating.
     return "x".to_string();
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::fs::{self, File};
-    use std::io::{self, Read};
-    use std::path::Path;
 
-    #[derive(Hash, PartialEq, Eq, Clone, Debug)]
-    struct TestCase {
-        fen_before: String,
-        input_fen: String,
-        output_fen: String,
-        fen_after: String,
-    }
+    use std::io::{self};
 
     #[test]
     fn test_from_test_data() -> io::Result<()> {
-        let test_data_dir = Path::new("test-data");
-        let mut count = 0;
-        let mut oks = 0;
-        for entry in fs::read_dir(test_data_dir)? {
-            let entry = entry?;
-            let path = entry.path();
-            if path.is_file() && path.file_name().and_then(|f| f.to_str()).map_or(false, |s| !s.starts_with('.')) {
-                let mut file = File::open(&path)?;
-                let mut contents = String::new();
-                file.read_to_string(&mut contents)?;
-                let parts: Vec<&str> = contents.split("\"").collect();
-                let mut test_case = TestCase {
-                    fen_before: String::new(),
-                    input_fen: String::new(),
-                    output_fen: String::new(),
-                    fen_after: String::new(),
-                };
-
-                for i in 0..parts.len() {
-                    match parts[i] {
-                        "fenBefore" => test_case.fen_before = parts[i + 2].replace('\\', ""),
-                        "inputFen" => test_case.input_fen = parts[i + 2].replace('\\', ""),
-                        "outputFen" => test_case.output_fen = parts[i + 2].replace('\\', ""),
-                        "fenAfter" => test_case.fen_after = parts[i + 2].replace('\\', ""),
-                        _ => {}
-                    }
-                }
-
-                let inputs = Input::array_from_fen(&test_case.input_fen);
-                let recreated_inputs_fen = Input::fen_from_array(&inputs);
-                assert!(recreated_inputs_fen == test_case.input_fen);
-
-                let game_after = MonsGame::from_fen(&test_case.fen_after);
-                let recreated_game_after_fen = game_after.unwrap().fen();
-                assert!(recreated_game_after_fen == test_case.fen_after);
-
-                let mut game_before = MonsGame::from_fen(&test_case.fen_before).unwrap();
-                let recreated_game_before_fen = game_before.fen();
-                assert!(recreated_game_before_fen == test_case.fen_before);
-
-                let output = Output::from_fen(&test_case.output_fen);
-                let recreated_output_fen = output.unwrap().fen();
-                assert!(recreated_output_fen == test_case.output_fen);
-
-                assert!(!test_case.fen_before.is_empty() && !test_case.fen_after.is_empty() && !test_case.output_fen.is_empty(), "test data must not be empty");
-
-                let actual_output = game_before.process_input(inputs, false, false);
-
-                if game_before.fen() != test_case.fen_after || actual_output.fen() != test_case.output_fen {
-                    println!("expected {}", test_case.output_fen);
-                    println!("received {}", actual_output.fen());
-                    println!("forinput {}", test_case.input_fen);
-                    println!("forboard {}", test_case.fen_before);
-                    println!("expected fen after {}", test_case.fen_after);
-                    println!("received fen after {}\n\n\n\n\n", game_before.fen());
-                    count += 1;
-                } else {
-                    oks += 1;
-                    println!("ok {}", oks);
-                }
-
-                assert!(game_before.fen() == test_case.fen_after);
-                assert!(actual_output.fen() == test_case.output_fen);
-            }
-        }
-        println!("\n\n\n\n\n TOTAL ERRORS {}", count);
-        println!("TOTAL OKS {}", oks);
+        let game = MonsGame::new();
+        let fen = game.fen();
+        assert!(fen == "0 0 w 0 0 0 0 0 1 n03y0xs0xd0xa0xe0xn03/n11/n11/n04xxmn01xxmn04/n03xxmn01xxmn01xxmn03/xxQn04xxUn04xxQ/n03xxMn01xxMn01xxMn03/n04xxMn01xxMn04/n11/n11/n03E0xA0xD0xS0xY0xn03");
         Ok(())
     }
 }
