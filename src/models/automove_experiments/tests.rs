6f0ee6d527dd16eb2acc32c30477913aa1b58c39
ivan grachev <ivan@ivan.lol>
2026-04-04 22:16:23 +0300

    docs: record dead clean reply screens

--- src/models/automove_experiments/tests.rs
+++ src/models/automove_experiments/tests.rs
@@ -6472,7 +6472,7 @@ fn smart_automove_pro_reliability_candidate_override_probe() {
     let mut normal_timing = DuelTimingStats::default();
 
     eprintln!(
-        "pro reliability candidate override probe config: candidate_profile={} baseline_profile={} seed_tag_pro={} seed_tag_normal={} repeats={} games_per_repeat={} max_plies={} include_acceptance={} override_turn_planner_root_injection={:?} override_turn_planner_root_injection_limit={:?} override_turn_planner_root_max_gap={:?} override_turn_planner_root_emergency_only={:?} override_secondary_analysis={:?} override_selected_followup_projection={:?} override_two_pass_root_allocation={:?} override_volatility_focus={:?} override_event_ordering={:?} override_selective_extensions={:?} override_normal_safety_rerank={:?} override_normal_safety_deep_floor={:?} override_low_budget_guard={:?} override_mid_turn_tactical_guard={:?} override_late_safe_mana_root_preference={:?}",
+        "pro reliability candidate override probe config: candidate_profile={} baseline_profile={} seed_tag_pro={} seed_tag_normal={} repeats={} games_per_repeat={} max_plies={} include_acceptance={} override_turn_planner_root_injection={:?} override_turn_planner_root_injection_limit={:?} override_turn_planner_root_max_gap={:?} override_turn_planner_root_emergency_only={:?} override_secondary_analysis={:?} override_selected_followup_projection={:?} override_two_pass_root_allocation={:?} override_volatility_focus={:?} override_event_ordering={:?} override_selective_extensions={:?} override_normal_safety_rerank={:?} override_normal_safety_deep_floor={:?} override_clean_reply={:?} override_hard_spirit_deploy={:?} override_soft_root_priors={:?} override_deterministic_tiebreak={:?} override_low_budget_guard={:?} override_mid_turn_tactical_guard={:?} override_late_safe_mana_root_preference={:?}",
         candidate_profile,
         baseline_profile,
         seed_tag_pro,
@@ -6493,6 +6493,10 @@ fn smart_automove_pro_reliability_candidate_override_probe() {
         env_bool("SMART_PROBE_FORCE_SELECTIVE_EXTENSIONS"),
         env_bool("SMART_PROBE_FORCE_NORMAL_SAFETY_RERANK"),
         env_bool("SMART_PROBE_FORCE_NORMAL_SAFETY_DEEP_FLOOR"),
+        env_bool("SMART_PROBE_FORCE_CLEAN_REPLY"),
+        env_bool("SMART_PROBE_FORCE_HARD_SPIRIT_DEPLOY"),
+        env_bool("SMART_PROBE_FORCE_SOFT_ROOT_PRIORS"),
+        env_bool("SMART_PROBE_FORCE_DETERMINISTIC_TIEBREAK"),
         env_bool("SMART_PROBE_FORCE_LOW_BUDGET_GUARD"),
         env_bool("SMART_PROBE_FORCE_MID_TURN_TACTICAL_GUARD"),
         env_bool("SMART_PROBE_FORCE_LATE_SAFE_MANA_ROOT_PREFERENCE"),
